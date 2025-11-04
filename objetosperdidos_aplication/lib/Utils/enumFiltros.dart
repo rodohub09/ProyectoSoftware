c941ee6b9f8a8b07bcb5f2ab054f466bd3fa2c5c
rodohub09 <superpro757@gmail.com>
2025-11-04 01:24:48 -0300

    Implementación Menu de Reportes y sistema para crear reportes

---
+++ objetosperdidos_aplication/lib/Utils/enumFiltros.dart
@@ -0,0 +1,12 @@
+enum Enumfiltros {
+  tecnologia('Tecnologia'),
+  vestimenta('Vestimenta'),
+  accesorios('Accesorios'),
+  documentos('Documentos'),
+  utiles('Utiles'),
+  otros('Otros');
+
+  final String label;
+  const Enumfiltros(this.label);
+}
+
