b72d9316db71d1cd949ec4d5ee2e365cc6fb8137
JP3dr3 <jpedreros2024@udec.cl>
2025-11-03 23:10:52 -0400

    Creacion Pantalla inicio.

---
+++ objetosperdidos_aplication/lib/ejemplo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Ejemplo extends StatelessWidget {
+  const Ejemplo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Placeholder(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Text("jyhfjhsfbhsfhsdb")],
+      ),
+    );
+  }
+}
